e89ecb2e7a319dafd7f4389afd83d319a2a37174
Eric Bratter <eric.bratter@gmail.com>
2019-12-01 12:46:15 -0500

    change to status code 200

--- src/services/staff-membership-request-update/src/success-mw.js
+++ src/services/staff-membership-request-update/src/success-mw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { CREATED } = require('../../../lib/constants').statusCodes;
+const { OK } = require('../../../lib/constants').statusCodes;
 
 module.exports = (req, res) => {
-  res.sendStatus(CREATED);
+  res.sendStatus(OK);
 };

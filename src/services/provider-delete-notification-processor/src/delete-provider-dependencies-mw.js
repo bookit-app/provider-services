3b5a5f913629d6f228169cadbb797e92aa9325dc
Eric Bratter <eric.bratter@gmail.com>
2019-12-01 13:26:16 -0500

    ensure staff member subcollection is cleaned up

--- src/services/provider-delete-notification-processor/src/delete-provider-dependencies-mw.js
+++ src/services/provider-delete-notification-processor/src/delete-provider-dependencies-mw.js
@@ -5,17 +5,20 @@ const ServiceError = require('../../../lib/util/service-error');
 const { clone } = require('lodash');
 
 /**
- * Express Middleware to trigger the creation of the
- * service in association the the provider. It assumes the data is pre-validated
+ * Express Middleware to trigger the deletion of the
+ * sub-collections associated with the provider. It assumes the data is pre-validated
  *
  * @param {Express.Request} req
  * @param {Express.Response} res
  * @param {Express.next} next
  * @returns
  */
-module.exports = repository => async (req, res, next) => {
+module.exports = repositories => async (req, res, next) => {
   try {
-    await repository.deleteAllForProvider(req.body.providerId);
+    await Promise.all(
+      repositories.map(repo => repo.deleteAllForProvider(req.body.providerId))
+    );
+
     next();
   } catch (err) {
     const error = clone(errors.updateFailed);
